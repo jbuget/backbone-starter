c5d348e0a0b295a5422f52b6170e42922c5b3325
Jérémy Buget <jbuget@octo.com>
2016-09-12 10:03:31 +0200

    Add some styles and tests

---
+++ test/unit/views/Home_test.js
@@ -0,0 +1,41 @@
+import { expect } from 'chai';
+import Pokemon from '../../../src/models/Pokemon';
+
+describe('Unit | Models | Pokemon', () => {
+
+  it('should exist', () => {
+    // given
+    const pokemon = new Pokemon({});
+    // then
+    expect(pokemon).to.exist;
+  });
+
+  describe('#initialize', () => {
+
+    it('should set property "pokedexId", extracted from attribute URL)', () => {
+      // given
+      const pokemon = new Pokemon({ url: 'http://pokeapi.co/api/v2/pokemon/1/', name: 'pikachu' });
+
+      // then
+      expect(pokemon.get('pokedexId')).to.equal('001');
+    });
+
+    it('should set property "pokedexId" to "N/A" if it can not be extracted from URL', () => {
+      // given
+      const pokemon = new Pokemon({ url: 'http://bad.url', name: 'pikachu' });
+
+      // then
+      expect(pokemon.get('pokedexId')).to.equal('N/A');
+    });
+
+    it('should set property "imageUrl" based on "assessts.pokemon.com" images endpoint', () => {
+      // given
+      const pokemon = new Pokemon({ url: 'http://pokeapi.co/api/v2/pokemon/1/', name: 'pikachu' });
+
+      // then
+      expect(pokemon.get('imageUrl')).to.equal('http://assets.pokemon.com/assets/cms2/img/pokedex/detail/001.png');
+    });
+
+  });
+
+});
